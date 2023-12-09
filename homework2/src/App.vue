dee0ad791887c453f79641b2806db0b67c0b7371
The_Valkyrie <110331611+Kirieshek@users.noreply.github.com>
2023-12-09 13:42:05 +0300

    Add files via upload

---
+++ homework2/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1 v-if="age < 4">Малыш</h1>
+  <h1 v-else-if="age > 3 & age < 13">Ребёнок</h1>
+  <h1 v-else-if="age < 19 & age > 12">Подросток</h1>
+  <h1 v-else-if="age > 18 & age < 26">Взрослый</h1>
+  <h1 v-else-if="age > 25 & age < 76">Старик</h1>
+  <h1 v-else>Призрак</h1>
+  <button @click="askage">Ответить на вопрос</button>
+
+</template>
+
+<script>
+export default {
+  methods: {
+    askage() {
+      this.age = prompt("Сколько вам лет?")
+    }
+  },
+  computed: {
+
+  },
+  data() {
+    return {
+      age: "",
+    }
+  },
+}
+</script>
+
+<style></style>
